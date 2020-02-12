2745c19040508ff5d6e9f7eab1254806a3801fe2
Vincent Kokojan <vincentkokojan@Vincents-MacBook-Pro.local>
2020-02-12 15:25:45 -0600

    added selection logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*----- constants -----*/
-const wheelArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
+
 /*----- app's state (variables) -----*/
 let currentBalance;
 let currentBet;
@@ -10,6 +10,7 @@ let spinWheel = document.getElementById('spinwheel');
 let wheelResult = document.getElementById('wheel');
 /*----- event listeners -----*/
 document.getElementById('placebet').addEventListener('click', adjustBalance);
+document.querySelector('.gameboard').addEventListener('click', boardSelection);
 resetGame.addEventListener('click', reset)
 spinWheel.addEventListener('click', getNumber)
 /*----- functions -----*/
@@ -17,9 +18,13 @@ init();
 
 function init(){
     currentBalance = 1000;
+    currentBet = enterbet.value;
     statusmessage.innerHTML = `Place your bets!!!`;
     statusmessage.style.backgroundColor = 'black';
-    renderBalance();
+    enterbet.value = '';
+    wheelResult.innerHTML = '';
+    
+    render();
 }
 
 function adjustBalance(){
@@ -37,25 +42,30 @@ function adjustBalance(){
         statusmessage.style.backgroundColor = 'black'
         statusmessage.innerHTML = `You have bet $${enterbet.value}! Choose a number!`
     }
-    renderBalance();
+    render();
 };
-function renderBalance(){
+function render(){
     balancemessage.textContent = currentBalance;
+    
+    
 }
 function reset(){
     init();
 }
-function boardSelection(event){
-    if (currentBalance.value <= 0) return;
-    if (enterbet.value > currentBalance.value) return;
+function boardSelection(number){
+    if (parseInt(currentBalance.value) <= 0) return;
+    if (parseInt(currentBet.value) > parseInt(currentBalance.value)) return;
     if (currentSelection) return;
-    currentSelection = event.target;
-    event.target.style.backgroundColor = '5px solid gold';
-}
+    currentSelection = number.target;
+    number.target.style.backgroundColor = 'gold';
+    statusmessage.style.color = 'gold';
+    statusmessage.style.backgroundColor = 'black'
+    statusmessage.innerHTML = `Spin the wheel!`;
+    
+};
 function getNumber(){
     if (parseInt(currentBalance.value) <= 0) return;
     if (parseInt(enterbet.value) > parseInt(currentBalance.value)) return;
     let wheelValue = (Math.floor(Math.random() * 36 +1));
     wheelResult.innerHTML = wheelValue;
-    wheelValue.style.color = 'gold';
-}
+};
